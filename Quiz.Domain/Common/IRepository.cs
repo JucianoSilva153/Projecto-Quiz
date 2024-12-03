defcbba0e0447814ec33a3ef9c3cc9f930fc35ab
jucianosilva153 <jucianodasilvasilvaa@gmail.com>
2024-12-03 23:49:28 +0100

    fix: change the colection return of the generic GetAllAsync method from List to IEnumerable

--- Quiz.Domain/Common/IRepository.cs
+++ Quiz.Domain/Common/IRepository.cs
@@ -4,7 +4,7 @@ public interface IRepository<T>
 {
     public Task<bool> CreateAsync(T entity);
     public Task<T> GetByIdAsync(Guid id);
-    public Task<List<T>> GetAllAsync();
+    public Task<IEnumerable<T>> GetAllAsync();
     public Task<bool> UpdateAsync(T entity);
     public Task DeleteAsync(Guid id);
 }
