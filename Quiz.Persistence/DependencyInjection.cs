75c45ae0254e3738b8da8de7c398ce93696bac4b
jucianosilva153 <jucianodasilvasilvaa@gmail.com>
2024-12-09 23:48:01 +0100

    chore: set DI

--- Quiz.Persistence/DependencyInjection.cs
+++ Quiz.Persistence/DependencyInjection.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Quiz.Domain.Entities.Accounts;
+using Quiz.Domain.Entities.Answers;
+using Quiz.Domain.Entities.Kwizzes;
+using Quiz.Domain.Entities.Points;
+using Quiz.Domain.Entities.Questions;
+using Quiz.Domain.Entities.Topics;
 using Quiz.Domain.Entities.Users;
+using Quiz.Persistence.Context;
 using Quiz.Persistence.Repositories;
 
 namespace Quiz.Persistence;
@@ -8,7 +16,24 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services)
     {
-        services.AddScoped<IUser, UserRepository>();
+        
+        services.AddDbContext<AppDbContext>(
+            options => options.UseMySql(
+                "server=localhost;database=kwiz;uid=root;",
+                ServerVersion.Parse("10.4.32")),
+            contextLifetime: ServiceLifetime.Transient,
+            optionsLifetime: ServiceLifetime.Singleton);
+        
+        services.AddTransient<IUser, UserRepository>();
+        services.AddTransient<IAccount, AccountRepository>();
+        services.AddTransient<IAnswer, AnswerRepository>();
+        services.AddTransient<IKwiz, KwizRepository>();
+        services.AddTransient<IPoint, PointRepository>();
+        services.AddTransient<IQuestion, QuestionRepository>();
+        services.AddTransient<ITopic, TopicRepository>();
+        
+        
+        
         return services;
     }
 }
