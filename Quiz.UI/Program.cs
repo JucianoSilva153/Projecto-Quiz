88f3f9a1f5ec298b7b1f60589efe2da55c953b26
jucianosilva153 <jucianodasilvasilvaa@gmail.com>
2024-12-11 19:39:13 +0100

    set mudblazor

--- Quiz.UI/Program.cs
+++ Quiz.UI/Program.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Quiz.Persistence;
 using Quiz.UI;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddPersistence();
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
